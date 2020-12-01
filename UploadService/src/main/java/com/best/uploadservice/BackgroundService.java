dfcadc761c5140eb1fc8f63bde3337b114ffb14b
Anonymous Submission <anonymous.submission@does-not-matter.com>
2020-12-01 13:23:25 +0100

    fixes

--- UploadService/src/main/java/com/best/uploadservice/BackgroundService.java
+++ UploadService/src/main/java/com/best/uploadservice/BackgroundService.java
@@ -2,8 +2,13 @@ package com.best.uploadservice;
 
 import android.app.IntentService;
 import android.content.Intent;
+import android.os.SystemClock;
+import android.util.Log;
 
 import androidx.annotation.Nullable;
+
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class BackgroundService extends IntentService {
     public BackgroundService(String name) {
@@ -16,6 +21,11 @@ public class BackgroundService extends IntentService {
 
     @Override
     protected void onHandleIntent(@Nullable Intent intent) {
-
+        new Timer().scheduleAtFixedRate(new TimerTask() {
+            @Override
+            public void run() {
+                Log.d("COVFEFE", "SystemTime: " + SystemClock.elapsedRealtime());
+            }
+        }, 0, 5000);
     }
 }
